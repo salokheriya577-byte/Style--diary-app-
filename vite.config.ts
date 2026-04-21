bc5c038f484c0c391bb7c6bd4b099638e3b07d7e
anya-byte577 <salokheriya577@gmail.com>
2026-04-22 01:41:44 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig, loadEnv} from 'vite';
 
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
+  base: '/Style--diary-app-/',
   return {
     plugins: [react(), tailwindcss()],
     define: {
