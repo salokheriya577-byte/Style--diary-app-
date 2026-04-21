3f9b5f3977edc8e5fff880d3c46e9984e0d409f4
anya-byte577 <salokheriya577@gmail.com>
2026-04-22 01:51:19 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,8 @@ import {defineConfig, loadEnv} from 'vite';
 
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
-  base: '/Style--diary-app-/',
   return {
+    base: '/Style--diary-app-/', 
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
